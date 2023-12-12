a4aebd7d23c8fdb7aaaf53fa492cabd3b3ca7637
Vi Tran ( Alice) <vi.tran@navercorp.com>
2023-12-12 11:29:39 +0700

    [update] structure storybook

--- src/components/ui/button/Button.stories.ts
+++ src/components/ui/button/Button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './Button';
+import { Button } from '.';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof Button> = {
@@ -21,30 +21,16 @@ const meta: Meta<typeof Button> = {
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
 export const Large: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
+    size: 'lg',
+    title: 'Button',
   },
 };
 
 export const Small: Story = {
   args: {
-    size: 'small',
-    label: 'Button',
+    size: 'sm',
+    title: 'Button',
   },
 };
