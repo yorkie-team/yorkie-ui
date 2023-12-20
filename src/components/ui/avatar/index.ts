9487dd1796493f665bcaa0a143e93dce6cc1f543
daisy142 <diep.tran@navercorp.com>
2023-12-20 13:21:20 +0700

    Add avatar component
    Use park-ui avatar component

---
+++ src/components/ui/avatar/index.ts
@@ -0,0 +1,15 @@
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Root,
+  Fallback,
+  Image,
+  AvatarProps,
+  AvatarFallbackProps,
+  AvatarImageProps,
+} from './Avatar';
+
+export { Avatar, AvatarFallback, AvatarImage };
+export { Root, Fallback, Image };
+export type { AvatarProps, AvatarFallbackProps, AvatarImageProps };
