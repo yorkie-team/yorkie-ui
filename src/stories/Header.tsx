aca3e5992e086cacf8bd3cc8bb147e9f9a586b1d
Vi Tran ( Alice) <vi.tran@navercorp.com>
2023-12-26 16:35:33 +0700

    update button

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -33,12 +33,12 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
+            <Button onClick={onLogout}> Log out</Button>
           </>
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <Button onClick={onLogin}> Log in</Button>
+            <Button onClick={onCreateAccount}>Sign up</Button>
           </>
         )}
       </div>
