d47e1dc86468e7b809f4a0b0d5830e609e225790
Vi Tran ( Alice) <vi.tran@navercorp.com>
2023-12-26 16:41:31 +0700

    add line last

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    bracketSpacing: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    printWidth: 120,
-    tabWidth: 2,
-  };
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
+};
