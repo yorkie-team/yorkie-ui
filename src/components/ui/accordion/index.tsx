bdcf460e3cec55153d2bb65ffe70ed47622fde05
daisy142 <diep.tran@navercorp.com>
2023-12-28 17:24:19 +0700

    Add accordion component
    Using park-ui component

---
+++ src/components/ui/accordion/index.tsx
@@ -0,0 +1,21 @@
+import {
+  Item,
+  ItemContent,
+  ItemIndicator,
+  ItemTrigger,
+  Root,
+  AccordionProps,
+  AccordionItemProps,
+  AccordionItemContentProps,
+  AccordionItemIndicatorProps,
+  AccordionItemTriggerProps,
+} from './Accordion';
+
+export { Item, ItemContent, ItemIndicator, ItemTrigger, Root };
+export type {
+  AccordionProps,
+  AccordionItemProps,
+  AccordionItemContentProps,
+  AccordionItemIndicatorProps,
+  AccordionItemTriggerProps,
+};
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,3 +2,4 @@ export * from './button';
 export * as Avatar from './avatar';
 export * as Checkbox from './checkbox';
 export * as RadioGroup from './radioGroup';
+export * as Accordion from './accordion';
