658e8542eb0c28a595b7194543075b69a11b8ad1
daisy142 <diep.tran@navercorp.com>
2023-12-28 09:29:39 +0700

    Add checkbox component
    Use park-ui checkbox component

---
+++ src/components/ui/checkbox/index.tsx
@@ -0,0 +1,17 @@
+import {
+  Checkbox,
+  CheckboxControl,
+  CheckboxIndicator,
+  CheckboxLabel,
+  Control,
+  Indicator,
+  Label,
+  Root,
+  CheckboxProps,
+  CheckboxControlProps,
+  CheckboxIndicatorProps,
+  CheckboxLabelProps,
+} from './Checkbox';
+
+export { Checkbox, CheckboxControl, CheckboxIndicator, CheckboxLabel, Control, Indicator, Label, Root };
+export type { CheckboxProps, CheckboxControlProps, CheckboxIndicatorProps, CheckboxLabelProps };
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,3 @@
 export * from './button';
 export * as Avatar from './avatar';
+export * as Checkbox from './checkbox';
