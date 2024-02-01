580de51c019b7f89f2ff34a649e13d13b97612dd
Vi Tran ( Alice) <vi.tran@navercorp.com>
2024-02-01 10:01:55 +0700

    update code

--- src/components/ui/select/Select.stories.tsx
+++ src/components/ui/select/Select.stories.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Select } from '@/components/ui';
 import { DownIcon } from '@/components/ui/icons';
+import { Button } from '@/components/ui';
 
 const meta = {
   title: 'COMMUNICATION / Select',
@@ -67,7 +68,132 @@ export const Customization = {
               <DownIcon />
             </Select.Indicator>
           </Select.Trigger>
+          <Select.ClearTrigger>
+            <Button variant="solid">Clear</Button>
+          </Select.ClearTrigger>
+        </Select.Control>
+        <Select.Positioner>
+          <Select.Content>
+            <Select.ItemGroup id="framework">
+              <Select.ItemGroupLabel htmlFor="framework">Frameworks</Select.ItemGroupLabel>
+              {items.map((item) => (
+                <Select.Item key={item.value} item={item}>
+                  <Select.ItemText>{item.label}</Select.ItemText>
+                  <Select.ItemIndicator>✓</Select.ItemIndicator>
+                </Select.Item>
+              ))}
+            </Select.ItemGroup>
+          </Select.Content>
+        </Select.Positioner>
+      </Select.Root>
+    );
+  },
+};
+
+export const Multiple = {
+  render: () => {
+    type Item = { label: string; value: string; disabled?: boolean };
+    const items: Item[] = [
+      { label: 'React', value: 'react' },
+      { label: 'Solid', value: 'solid' },
+      { label: 'Vue', value: 'vue' },
+      { label: 'Svelte', value: 'svelte', disabled: true },
+    ];
+
+    return (
+      <Select.Root items={items} multiple>
+        <Select.Label>Framework</Select.Label>
+        <Select.Control>
+          <Select.Trigger>
+            <Select.ValueText placeholder="Select a Framework" />
+            <Select.Indicator>
+              <DownIcon />
+            </Select.Indicator>
+          </Select.Trigger>
           <Select.ClearTrigger>Clear</Select.ClearTrigger>
+        </Select.Control>
+        <Select.Positioner>
+          <Select.Content>
+            <Select.ItemGroup id="framework">
+              <Select.ItemGroupLabel htmlFor="framework">Frameworks</Select.ItemGroupLabel>
+              {items.map((item) => (
+                <Select.Item key={item.value} item={item}>
+                  <Select.ItemText>{item.label}</Select.ItemText>
+                  <Select.ItemIndicator>✓</Select.ItemIndicator>
+                </Select.Item>
+              ))}
+            </Select.ItemGroup>
+          </Select.Content>
+        </Select.Positioner>
+      </Select.Root>
+    );
+  },
+};
+
+export const Controlled = () => {
+  type Item = { label: string; value: string; disabled?: boolean };
+  const [, setSelectedItems] = useState<Item[]>([]);
+  const items: Item[] = [
+    { label: 'React', value: 'react' },
+    { label: 'Solid', value: 'solid' },
+    { label: 'Vue', value: 'vue' },
+    { label: 'Svelte', value: 'svelte', disabled: true },
+  ];
+
+  return (
+    <>
+      <Select.Root items={items} onValueChange={(e) => setSelectedItems(e.items)}>
+        <Select.Label>Framework</Select.Label>
+        <Select.Control>
+          <Select.Trigger>
+            <Select.ValueText placeholder="Select a Framework" />
+            <Select.Indicator>
+              <DownIcon />
+            </Select.Indicator>
+          </Select.Trigger>
+          <Select.ClearTrigger>Clear</Select.ClearTrigger>
+        </Select.Control>
+        <Select.Positioner>
+          <Select.Content>
+            <Select.ItemGroup id="framework">
+              <Select.ItemGroupLabel htmlFor="framework">Frameworks</Select.ItemGroupLabel>
+              {items.map((item) => (
+                <Select.Item key={item.value} item={item}>
+                  <Select.ItemText>{item.label}</Select.ItemText>
+                  <Select.ItemIndicator>✓</Select.ItemIndicator>
+                </Select.Item>
+              ))}
+            </Select.ItemGroup>
+          </Select.Content>
+        </Select.Positioner>
+      </Select.Root>
+    </>
+  );
+};
+
+export const UsageWithAFormLibrary = {
+  render: () => {
+    type Inputs = {
+      framework: string;
+    };
+    const items = ['React', 'Solid', 'Vue'];
+    const { control, handleSubmit } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+    return (
+      <Select.Root items={items}>
+        <Select.Label>Framework</Select.Label>
+        <Select.Control>
+          <Select.Trigger>
+            <Select.ValueText placeholder="Select a Framework" />
+            <Select.Indicator>
+              <DownIcon />
+            </Select.Indicator>
+          </Select.Trigger>
+          <Select.ClearTrigger>
+            <Button variant="solid">Clear</Button>
+          </Select.ClearTrigger>
         </Select.Control>
         <Select.Positioner>
           <Select.Content>
