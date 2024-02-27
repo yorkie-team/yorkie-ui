a9a04d6df8a5a683206d2c6195ef3840870e8566
Vi Tran ( Alice) <vi.tran@navercorp.com>
2024-02-27 17:43:56 +0700

    update select stories

--- src/components/ui/select/Select.stories.tsx
+++ src/components/ui/select/Select.stories.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Select } from '@/components/ui';
 import { DownIcon } from '@/components/ui/icons';
-import { Button } from '@/components/ui';
 
 const meta = {
   title: 'COMMUNICATION / Select',
@@ -221,9 +220,7 @@ export const MultipleSelection = {
               <DownIcon />
             </Select.Indicator>
           </Select.Trigger>
-          <Select.ClearTrigger>
-            <Button variant="solid">Clear</Button>
-          </Select.ClearTrigger>
+          <Select.ClearTrigger>Clear</Select.ClearTrigger>
         </Select.Control>
         <Select.Positioner>
           <Select.Content>
@@ -241,87 +238,4 @@ export const MultipleSelection = {
       </Select.Root>
     );
   },
-};
-
-export const Multiple = {
-  render: () => {
-    type Item = { label: string; value: string; disabled?: boolean };
-    const items: Item[] = [
-      { label: 'React', value: 'react' },
-      { label: 'Solid', value: 'solid' },
-      { label: 'Vue', value: 'vue' },
-      { label: 'Svelte', value: 'svelte', disabled: true },
-    ];
-
-    return (
-      <Select.Root items={items} multiple width="2xs">
-        <Select.Label>Framework</Select.Label>
-        <Select.Control>
-          <Select.Trigger>
-            <Select.ValueText placeholder="Select a Framework" />
-            <Select.Indicator>
-              <DownIcon />
-            </Select.Indicator>
-          </Select.Trigger>
-          <Select.ClearTrigger>
-            <Button variant="solid">Clear</Button>
-          </Select.ClearTrigger>
-        </Select.Control>
-        <Select.Positioner>
-          <Select.Content>
-            <Select.ItemGroup id="framework">
-              <Select.ItemGroupLabel htmlFor="framework">Frameworks</Select.ItemGroupLabel>
-              {items.map((item) => (
-                <Select.Item key={item.value} item={item}>
-                  <Select.ItemText>{item.label}</Select.ItemText>
-                  <Select.ItemIndicator>✓</Select.ItemIndicator>
-                </Select.Item>
-              ))}
-            </Select.ItemGroup>
-          </Select.Content>
-        </Select.Positioner>
-      </Select.Root>
-    );
-  },
-};
-
-export const Controlled = () => {
-  type Item = { label: string; value: string; disabled?: boolean };
-  const [, setSelectedItems] = useState({});
-  const items: Item[] = [
-    { label: 'React', value: 'react' },
-    { label: 'Solid', value: 'solid' },
-    { label: 'Vue', value: 'vue' },
-    { label: 'Svelte', value: 'svelte', disabled: true },
-  ];
-
-  return (
-    <Select.Root items={items} width="2xs" onValueChange={(e) => setSelectedItems(e.items)}>
-      <Select.Label>Framework</Select.Label>
-      <Select.Control>
-        <Select.Trigger>
-          <Select.ValueText placeholder="Select a Framework" />
-          <Select.Indicator>
-            <DownIcon />
-          </Select.Indicator>
-        </Select.Trigger>
-        <Select.ClearTrigger>
-          <Button variant="solid">Clear</Button>
-        </Select.ClearTrigger>
-      </Select.Control>
-      <Select.Positioner>
-        <Select.Content>
-          <Select.ItemGroup id="framework">
-            <Select.ItemGroupLabel htmlFor="framework">Frameworks</Select.ItemGroupLabel>
-            {items.map((item) => (
-              <Select.Item key={item.value} item={item}>
-                <Select.ItemText>{item.label}</Select.ItemText>
-                <Select.ItemIndicator>✓</Select.ItemIndicator>
-              </Select.Item>
-            ))}
-          </Select.ItemGroup>
-        </Select.Content>
-      </Select.Positioner>
-    </Select.Root>
-  );
 };
