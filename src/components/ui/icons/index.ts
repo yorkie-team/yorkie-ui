1bd065aa4c2c74e99ef2b4a15c58ac0cee90a628
Alice [ UI Engineer ] <150248298+vitran12@users.noreply.github.com>
2024-02-28 17:46:11 +0700

    update pagination (#69)

--- src/components/ui/icons/index.ts
+++ src/components/ui/icons/index.ts
@@ -3,5 +3,7 @@ import { CheckIcon } from './check-icon';
 import { MinusIcon } from './minus-icon';
 import { DownIcon } from './down-icon';
 import { CloseIcon } from './close-icon';
+import { ChevronLeftIcon } from './chevron-left-icon';
+import { ChevronRightIcon } from './chevron-right-icon';
 
-export { UserIcon, CheckIcon, MinusIcon, DownIcon, CloseIcon };
+export { UserIcon, CheckIcon, MinusIcon, DownIcon, CloseIcon, ChevronLeftIcon, ChevronRightIcon };
