9afffe2ff9f3ed3502766fe82f611bb35d39f5ae
Vi Tran ( Alice) <vi.tran@navercorp.com>
2024-03-13 13:42:28 +0700

    updated

--- src/components/ui/label/Label.tsx
+++ src/components/ui/label/Label.tsx
@@ -1,7 +1,7 @@
 import { ark } from '@ark-ui/react/factory';
 import type { ComponentProps } from 'react';
 import { styled } from '@/styled/jsx';
-import { label } from '@/styled/recipes';
+import { formLabel } from '@/styled/recipes';
 
-export const Label = styled(ark.label, label);
+export const Label = styled(ark.label, formLabel);
 export interface FormProps extends ComponentProps<typeof Label> {}
