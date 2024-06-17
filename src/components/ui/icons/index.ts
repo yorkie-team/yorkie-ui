8874edc72a0abec4c4a3149e488e86f42e15c9ae
Vi Tran ( Alice) <vi.tran@navercorp.com>
2024-06-17 17:51:36 +0700

    update code homepage

--- src/components/ui/icons/index.ts
+++ src/components/ui/icons/index.ts
@@ -6,5 +6,66 @@ import { CloseIcon } from './close-icon';
 import { ChevronLeftIcon } from './chevron-left-icon';
 import { ChevronRightIcon } from './chevron-right-icon';
 import { CalendarIcon } from './calendar';
+import { ScenarioIcon } from './icon_scenario';
+import { DiamondIcon } from './icon_diamond';
+import { StarIcon } from './star';
+import { BookIcon } from './icon_book';
+import { PackageIcon } from './icon_package';
+import { CloudIcon } from './icon_cloud_orange';
+import { TwinkleIcon } from './icon_twinkle';
+import { SmileIcon } from './icon_smile';
+import { MessageSquareIcon } from './icon_message_square';
+import { GitHubIcon } from './icon_github';
+import { SlackIcon } from './icon_slack';
+import { BackHomeIcon } from './icon_back_home';
+import { ArrowIcon } from './icon_arrow';
+import { DiscordIcon } from './icon_discord';
+import { CopyIcon } from './icon_copy';
+import { GnbMenuIcon } from './icon_gnb_menu';
+import { CloseSmallIcon } from './icon_close_small';
+import { FolderCloseIcon } from './icon_folder';
+import { FolderOpenIcon } from './icon_folder_open';
+import { FileIcon } from './icon_file';
+import { PlusIcon } from './icon_plus';
+import { ExpandIcon } from './icon_expand';
+import { MinimizeIcon } from './icon_minimize';
+import { ArrowLeftIcon } from './icon_arrow_left';
+import { ArrowRightIcon } from './icon_arrow_right';
+import { LogoIcon } from './logo';
 
-export { UserIcon, CheckIcon, MinusIcon, DownIcon, CloseIcon, ChevronLeftIcon, ChevronRightIcon, CalendarIcon };
+export {
+  UserIcon,
+  CheckIcon,
+  MinusIcon,
+  DownIcon,
+  CloseIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  CalendarIcon,
+  StarIcon,
+  CopyIcon,
+  BookIcon,
+  BackHomeIcon,
+  SlackIcon,
+  GitHubIcon,
+  GnbMenuIcon,
+  CloseSmallIcon,
+  DiamondIcon,
+  MessageSquareIcon,
+  SmileIcon,
+  ScenarioIcon,
+  ArrowIcon,
+  TwinkleIcon,
+  DiscordIcon,
+  ExpandIcon,
+  PlusIcon,
+  MinimizeIcon,
+  PackageIcon,
+  CloudIcon,
+  FolderCloseIcon,
+  FolderOpenIcon,
+  FileIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  LogoIcon,
+};
