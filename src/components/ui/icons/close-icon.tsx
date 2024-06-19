ceaf3355193e75a2d6085d76188a9601f41cffae
Vi Tran ( Alice) <vi.tran@navercorp.com>
2024-06-19 15:44:20 +0700

    update button and name prop button

--- src/components/ui/icons/close-icon.tsx
+++ src/components/ui/icons/close-icon.tsx
@@ -13,8 +13,8 @@ export const IconClose = (props: any) => (
     <path
       d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426"
       strokeWidth="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     ></path>
   </svg>
 );
