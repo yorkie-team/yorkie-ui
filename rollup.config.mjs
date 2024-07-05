b9603460c29734283c3ce347df828d23ce934131
Vi Tran ( Alice) <vi.tran@navercorp.com>
2024-07-05 12:14:07 +0700

    update config build package

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import tsConfigPaths from 'rollup-plugin-tsconfig-paths';
 import typescript from 'rollup-plugin-typescript2';
 import babel from '@rollup/plugin-babel';
