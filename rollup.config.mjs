47921ac842f7eeb1e9f44298af34e0a2030bdac3
Vi Tran ( Alice) <vi.tran@navercorp.com>
2024-07-07 15:33:58 +0700

    update  build package

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,7 +7,7 @@ import babel from '@rollup/plugin-babel';
 import image from '@rollup/plugin-image';
 import preserveDirectives from 'rollup-plugin-preserve-directives';
 import svgr from '@svgr/rollup';
-
+import packageJson from './package.json';
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
@@ -20,22 +20,31 @@ export default {
     { dir: 'dist/esm', format: 'esm', preserveModules: true },
   ],
   plugins: [
+    image(),
     tsConfigPaths(), // Ensure this comes before nodeResolve
     nodeResolve({
       extensions: ['.tsx', '.ts', '.jsx', '.js', '.json', '.svg'],
       browser: true, // This option might be needed depending on your setup
     }),
     typescript({ useTsconfigDeclarationDir: true }),
+    typescript({
+      exclude: ['**/*.stories.tsx', '**/*.mdx'], // loại trừ các tệp .stories.tsx và .mdx
+    }),
     babel({
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
       babelHelpers: 'bundled',
       presets: ['@babel/preset-react'],
     }),
+    svgr(),
     commonjs(), // converts date-fns to ES modules
     preserveDirectives(),
     production && terser(), // minify, but only in production
-    image(),
-    svgr(),
   ],
-  external: ['react', 'react-dom', 'prop-types'],
+  external: [
+    'react',
+    'react-dom',
+    'prop-types',
+    ...Object.keys(packageJson.dependencies || {}),
+    ...Object.keys(packageJson.peerDependencies || {}),
+  ],
 };
