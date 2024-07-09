735709896db9d2ed80cf77a254143532fd1532e3
Vi Tran ( Alice) <vi.tran@navercorp.com>
2024-07-09 17:13:07 +0700

    change build package tsc -> rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,42 +8,54 @@ import image from '@rollup/plugin-image';
 import preserveDirectives from 'rollup-plugin-preserve-directives';
 import svgr from '@svgr/rollup';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-
-const production = !process.env.ROLLUP_WATCH;
+import dts from 'rollup-plugin-dts';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  input: 'src/index.ts',
-  output: [
-    { dir: 'dist/cjs', format: 'cjs', sourcemap: true, exports: 'named' },
-    {
-      dir: 'dist/esm',
-      format: 'esm',
-      preserveModules: true,
-      sourcemap: true,
-      exports: 'named',
-      preserveModulesRoot: 'src',
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    tsConfigPaths(), // Ensure this comes before nodeResolve
-    nodeResolve({
-      extensions: ['.tsx', '.ts', '.jsx', '.js', '.json', '.svg'],
-      browser: true, // Adjust based on your setup
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      babelHelpers: 'bundled',
-      presets: ['@babel/env', '@babel/preset-react'],
-    }),
-    typescript(),
-    image(),
-    svgr(),
-    commonjs(), // Convert CommonJS modules to ES6
-    preserveDirectives(),
-    production && terser(), // Minify in production
-  ],
-  external: ['react', 'react-dom', 'prop-types'], // Exclude these from bundle
-};
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      { dir: 'dist/cjs', format: 'cjs', sourcemap: true, exports: 'named' },
+      {
+        dir: 'dist/esm',
+        format: 'esm',
+        sourcemap: true,
+        exports: 'named',
+        preserveModulesRoot: 'src',
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      tsConfigPaths(), // Ensure this comes before nodeResolve
+      nodeResolve({
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '.json', '.svg'],
+        browser: true, // Adjust based on your setup
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        babelHelpers: 'bundled',
+        presets: ['@babel/env', '@babel/preset-react'],
+      }),
+      typescript({ tsconfig: './tsconfig.json', exclude: ['**/src/stories/**', '**/*.stories.tsx'] }),
+      image(),
+      svgr(),
+      commonjs(), // Convert CommonJS modules to ES6
+      preserveDirectives(),
+      terser(), // Minify in production
+    ],
+    external: ['react', 'react-dom', 'prop-types'], // Exclude these from bundle
+  },
+  {
+    input: './src/index.ts',
+    output: [{ dir: 'dist/types', format: 'es' }],
+    plugins: [
+      peerDepsExternal(),
+      tsConfigPaths(), // Ensure this comes before nodeResolve
+      image(),
+      svgr(),
+      dts(),
+    ],
+    external: ['react', 'react-dom', 'prop-types'], // Exclude these from bundle
+  },
+];
