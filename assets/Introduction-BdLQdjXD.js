6cd7b0bc4cd97bbc4673b6aee7c7a31a80867cf2
hackerwins <hackerwins@users.noreply.github.com>
2024-08-06 06:43:18 +0000

    deploy: b47f256aa07654994de72f60c373d35866adb6e7

--- assets/Introduction-BdLQdjXD.js
+++ assets/Introduction-BdLQdjXD.js
@@ -1,4 +1,4 @@
-import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as s}from"./index-Bvgtear9.js";import"./index-RYns6xqu.js";import"./iframe-DUGoF5rY.js";import"../sb-preview/runtime.js";import"./index-D16Yfzz8.js";import"./index-D-8MO0q_.js";import"./index-B23dhaOI.js";import"./index-DrFu-skq.js";function o(t){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",strong:"strong",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Introduction"}),`
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as s}from"./index-B6o932E0.js";import"./index-RYns6xqu.js";import"./iframe-B36yNtpn.js";import"../sb-preview/runtime.js";import"./index-D16Yfzz8.js";import"./index-D-8MO0q_.js";import"./index-B23dhaOI.js";import"./index-DrFu-skq.js";function o(t){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",strong:"strong",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Introduction"}),`
 `,n.jsx(e.h1,{id:"get-started",children:"Get started"}),`
 `,n.jsx(e.p,{children:"Yorkie UI is a component library that helps build UIs faster. The goal is to make building UIs more productive."}),`
 `,n.jsx(e.h2,{id:"install",children:"Install"}),`
@@ -7,7 +7,7 @@ import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"
 `,n.jsx(e.pre,{children:n.jsx(e.code,{children:`npm install --save @yorkie-ui/core
 `})}),`
 `,n.jsx(e.p,{children:"Add styles imports to the root of your project."}),`
-`,n.jsx(e.pre,{children:n.jsx(e.code,{children:`import '@yorkie-ui/core/dist/styles.css';
+`,n.jsx(e.pre,{children:n.jsx(e.code,{children:`import '@yorkie-ui/core/styles.css';
 
 `})}),`
 `,n.jsx(e.h3,{id:"use",children:n.jsx(e.strong,{children:"Use"})}),`
