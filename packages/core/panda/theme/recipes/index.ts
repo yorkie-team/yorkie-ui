df5ef7e361c72c975c9ed167c2a2f85b9c754ddb
Youngteac Hong <susukang98@gmail.com>
2024-08-14 18:28:34 +0900

    Add Select component to core package

--- packages/core/panda/theme/recipes/index.ts
+++ packages/core/panda/theme/recipes/index.ts
@@ -1,6 +1,12 @@
-import type { RecipeConfig, RecipeVariantRecord } from '@pandacss/dev';
+import type { RecipeConfig, RecipeVariantRecord, SlotRecipeConfig } from '@pandacss/dev';
 
 import { button } from './button';
+import { select } from './select';
+
 export const recipes: Record<string, Partial<RecipeConfig<RecipeVariantRecord>>> = {
   button,
+};
+
+export const slotRecipes: Record<string, Partial<SlotRecipeConfig>> = {
+  select,
 };
